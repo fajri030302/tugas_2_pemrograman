fc46356429b593aaceb77b58238ceb31fc1535aa
fajri030302 <yelfajri4@gmail.com>
2021-11-19 01:01:21 +0700

    percobaan dan latihann

---
+++ P2_1_1204020/P2_1_1204020/P2_1_1204020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204020
+
+{
+    class ProductTest_1204016
+    {
+        static void Main(string[] args)
+        {
+
+
+            book_1204020 mybook = new book_1204020("Book", "C# Object Oriented Solution", "300");
+            DVD_1204020 dvd = new DVD_1204020("Ethernal Sunshine Of The Spotless Mind", "145");
+
+            Console.WriteLine("Product 1 is a {0} called \"{1}\" and has {2} pages", mybook.MyType, mybook.MyTitle, mybook.PageCount);
+            Console.WriteLine("Product 1 is a {0} called \"{1}\" and has {2} minutes duration", dvd.MyType, dvd.MyTitle, dvd.Duration);
+        }
+    }
+}
+
+
---
+++ P2_1_1204020/P2_1_1204020/P2_1_1204020/book_1204020.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204020
+{
+    public class book_1204020 : Product_1204020
+    {
+        protected string pageCount;
+
+        public book_1204020(string Mytype, string title, string pagecount) : base(Mytype, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+
+
+            set
+            {
+                pageCount = value;
+            }
+
+
+        }
+
+    }
+}
